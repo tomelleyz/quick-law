b900a6ff8fcd2f20a93df038bb76e2afea67341d
Ola Eleyinmi <eleyinmiolasunkanmi@gmail.com>
2020-03-28 09:21:19 +0100

    Styled the menu and fixed some responsiveness issue.

--- js/main.js
+++ js/main.js
@@ -8,23 +8,22 @@ function scrollFunction() {
   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     $(".nav-bar").css({"background": "#ffffff", "padding": "1em 4.8vw"})
     .addClass("shadow-sm");
+    $("navbar-search-field").css("border", "1px solid #000000");
   } else {
     $('.nav-bar').css({"background": "transparent", "padding": "1.5em 5.8vw"})
     .removeClass("shadow-sm");
+    $(".navbar-search-field").css("border", "none");
   }
 }
 
 // Function that opens Mobile Nav 
 $(".menu-icon").click(function(){
-    $(".nav-bar").fadeOut("fast", function() {
-      $(".menu").css("top", 0);
-    });
+    $(".menu").slideDown("fast");
 });
 
 // Function that closes Mobile Nav
 $(".close-menu-icon").click(function(){
-    $(".menu").css("top", "-100%")
-    $(".nav-bar").fadeIn("slow");
+    $(".menu").slideUp("fast");
 });
 
 });
